e6f30b32c6b6bfcce24c958a72ed5d65505860ef
germanalvarez8 <ger.22.alvarez@gmail.com>
2025-04-08 20:12:57 -0300

    view

--- src/unicorns/UnicornsView.jsx
+++ src/unicorns/UnicornsView.jsx
@@ -7,95 +7,6 @@ const UnicornsView = ({
     onCreateUnicorn,
     onDeleteUnicorn
 }) => {
-    const [newUnicornName, setNewUnicornName] = useState('');
-
-    const [formData, setFormData] = useState({
-        name: '',
-        color: '',
-        age: 0,
-        power: ''
-    })
-
-    const handleInputChange = (e) => {
-        const { name, value } = e.target
-        setFormData(prev => ({
-          ...prev,
-          [name]: value
-        }))
-      }
-
-    const [editingProduct, setEditingProduct] = useState(null)
-
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-        if (editingProduct) {
-            console.log('editalo', editingProduct)
-            try {
-                const response = await fetch(`https://api.restful-api.dev/objects/${editingProduct.id}`, {
-                    method: 'PUT',
-                    headers: {
-                    'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify({
-                    name: formData.name,
-                    data: {
-                        features: formData.features,
-                        price: Number(formData.price),
-                        year: Number(formData.year)
-                    }
-                    })
-                })
-
-                const updatedProduct = await response.json()
-                console.log('Objeto actualizado:', updatedProduct)
-                setProducts(products.map(product => product.id === editingProduct.id ? updatedProduct : product))
-                setEditingProduct(null)
-                setFormData({ name: '', features: '', price: 0, year: 0 })
-
-            } catch (error) {
-            console.error('Error:', error)
-            alert('Hubo un error al actualizar el objeto. Por favor, intente nuevamente.')
-            }
-        } else {
-            console.log('crealo')
-            try {
-            const response = await fetch('https://api.restful-api.dev/objects', {
-                method: 'POST',
-                headers: {
-                'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                name: formData.name,
-                data: {
-                    features: formData.features,
-                    price: Number(formData.price),
-                    year: Number(formData.year)
-                }
-                })
-            })
-
-            if (!response.ok) {
-                throw new Error('Error al crear el objeto')
-            }
-
-            const newProduct = await response.json()
-            console.log('Objeto creado:', newProduct)
-
-            const updatedProducts = [...products, newProduct]
-            setProducts(updatedProducts)
-            localStorage.setItem('products', JSON.stringify(updatedProducts))
-
-            setFormData({ name: '', features: '', price: 0, year: 0 })
-            } catch (error) {
-            console.error('Error:', error)
-            alert('Hubo un error al crear el objeto. Por favor, intente nuevamente.')
-            }
-        }
-    }
-
-    if (loading) return <div>Cargando unicornios...</div>;
-    if (error) return <div className="error">{error}</div>;
-
     return (
         <div className="container">
             <h1>Gestión de Inventario</h1>
@@ -154,7 +65,7 @@ const UnicornsView = ({
                     </thead>
                     <tbody>
                         {unicorns.map(unicorn => (
-                        <tr key={unicorn.id}>
+                        <tr key={unicorn._id}>
                             <td>{unicorn.name}</td>
                             <td>{unicorn.colour}</td>
                             <td>{unicorn.age}</td>
